e3a83d6335be732af6309fd9aaffe814104b71e4
Leonardo Freite <lfreite@atlanticqi.com>
2025-07-10 18:48:52 -0500

    Feat: Swipe implement

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonicModule,CommonModule],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
   constructor() {}
