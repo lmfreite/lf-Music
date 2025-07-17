b130ba372351ae866cb03540bb36cf6484eebcc3
Leonardo Freite <77871584+lmfreite@users.noreply.github.com>
2025-07-16 23:38:19 -0500

    feat: integrate Tailwind CSS and enhance HomePage layout
    - Added Tailwind CSS and PostCSS dependencies to package.json.
    - Updated home.page.html to utilize Tailwind CSS for styling and improved layout.
    - Removed home.page.spec.ts as it is no longer needed.
    - Enhanced home.page.ts to include a genres array for dynamic content in the swiper.
    - Modified global.scss to import Tailwind CSS styles.
    - Created a .postcssrc.json configuration file for PostCSS with Tailwind CSS plugin.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,5 +10,26 @@ import { IonicModule } from '@ionic/angular';
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePage {
+genres = [
+    {
+      title: 'Musica Clasica',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ1XWg6rXCY6YfiJy4VcJpSMEw1xtItavvGQ&s',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad labore suscipit a facere asperiores ratione maxime deleniti doloremque rerum quidem expedita explicabo cupiditate consectetur vero vel est, quo.',
+    },
+    {
+      title: 'Musica Pop',
+      image: 'https://s1.significados.com/foto/musica-pop-og.jpg',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad labore suscipit a facere asperiores ratione maxime deleniti doloremque rerum quidem expedita explicabo cupiditate consectetur vero vel est, quo.',
+    },
+    {
+      title: 'Musica Rock',
+      image: 'https://johanycarlos.home.blog/wp-content/uploads/2019/01/depositphotos_67643805-stock-illustration-rock-music-concept.jpg',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad labore suscipit a facere asperiores ratione maxime deleniti doloremque rerum quidem expedita explicabo cupiditate consectetur vero vel est, quo.',
+    }
+  ];
+
   constructor() {}
 }
