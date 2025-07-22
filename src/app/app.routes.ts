8d8815274d3c3ab746f0c7816f057a1618d66a51
Leonardo Freite <77871584+lmfreite@users.noreply.github.com>
2025-07-22 02:45:24 -0500

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,25 @@
 import { Routes } from '@angular/router';
+import { introGuard } from './guards/intro.guard';
 
 export const routes: Routes = [
   {
+    path: 'intro',
+    loadComponent: () => import('./intro/intro.page').then( m => m.IntroPage)
+  },
+  {
     path: '',
     loadComponent: () => import('./components/tabs/tabs.component').then(m => m.TabsComponent),
+    canActivate: [introGuard],
     children: [
       {
         path: '',
         loadComponent: () => import('./components/layout/layout.component').then(m => m.LayoutComponent),
         children: [
+          {
+            path: '',
+            redirectTo: 'home',
+            pathMatch: 'full'
+          },
           {
             path: 'home',
             loadComponent: () => import('./home/home.page').then(m => m.HomePage),
@@ -24,14 +35,14 @@ export const routes: Routes = [
           {
             path: 'search',
             loadComponent: () => import('./components/search/search.component').then(m => m.SearchComponent),
-          },
-          {
-            path: '**',
-            redirectTo: 'home',
-            pathMatch: 'full',
-          },
+          }
         ]
       }
     ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
   }
 ];
